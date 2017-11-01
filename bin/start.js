55d23bb1843047182d92a0115193d9fde30a9830
feugy <damien.feugas@gmail.com>
2017-11-01 09:26:43 +0100

    Fixes dots in contest results, loads configuration from file

--- bin/start.js
+++ bin/start.js
@@ -1,33 +1,17 @@
 // read env variables
 require('dotenv').config()
+const {join} = require('path')
+const {safeLoad} = require('js-yaml')
+const {readFileSync} = require('fs')
+const {merge} = require('lodash/fp')
 const startServer = require('../')
 
-startServer({
+startServer(merge({
   port: process.env.PORT,
-  logLevel: 'info',
-  // storage options
   storage: {
-    name: 'Mongo',
     url: process.env.MONGO_URL
   },
-  // provider options
-  providers: [{
-    name: 'FFDS',
-    url: 'http://dansesportive.ffdanse.fr',
-    list: 'compet-resultats.php',
-    details: 'compet-resultats.php?NumManif=%1$s',
-    clubs: 'compet-situation.php',
-    couples: 'compet-situation.php?club_id=%1s&Recherche_Club=',
-    search: 'compet-situation.php?couple_name=%1$s&Recherche_Nom=',
-    dateFormat: 'DD/MM/YYYY'
-  }, {
-    name: 'WDSF',
-    url: 'http://www.worlddancesport.org',
-    list: 'Calendar/Competition/Results?format=csv&downloadFromDate=01/01/%1$s&downloadToDate=31/12/%1$s&kindFilter=Competition',
-    dateFormat: 'YYYY/MM/DD'
-  }],
-  // authentication options
   auth: {
     key: process.env.JWT_KEY
   }
-})
+})(safeLoad(readFileSync(join('conf', 'default.yml')))))
